82ae45e376a694fd41701ec431d0b784a053fb4e
Jacob Gordon <jacobhicks123123@gmail.com>
2020-11-23 17:57:59 -0400

    Add BinaryExtension functions

---
+++ BinaryExtensions/BinaryReaderExtensions.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BinaryExtensions
+{
+    public static class BinaryReaderExtensions
+    {
+        public static void Seek(this BinaryReader reader, uint offset)
+        {
+            reader.BaseStream.Position = (long)offset;
+        }
+        public static void Seek(this BinaryReader reader, long offset)
+        {
+            reader.BaseStream.Position = offset;
+        }
+        public static string ReadString(this BinaryReader reader, uint count)
+        {
+            string ret = string.Empty;
+            for (uint i = 0; i < count; ++i)
+                ret += (char)reader.ReadByte();
+            return ret;
+        }
+        public static bool ReadBooleanAt(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            bool ret = reader.ReadBoolean();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static byte ReadByteAt(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            byte ret = reader.ReadByte();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static sbyte ReadSByteAt(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            sbyte ret = reader.ReadSByte();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static byte[] ReadBytesAt(this BinaryReader reader, int count, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            byte[] ret = reader.ReadBytes(count);
+            reader.Seek(prev);
+            return ret;
+        }
+        public static short ReadInt16At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            short ret = reader.ReadInt16();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static ushort ReadUInt16At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            ushort ret = reader.ReadUInt16();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static int ReadInt32At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            int ret = reader.ReadInt32();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static uint ReadUInt32At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            uint ret = reader.ReadUInt32();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static long ReadInt64At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            long ret = reader.ReadInt64();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static ulong ReadUInt64At(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            ulong ret = reader.ReadUInt64();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static float ReadSingleAt(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            float ret = reader.ReadSingle();
+            reader.Seek(prev);
+            return ret;
+        }
+        public static double ReadDoubleAt(this BinaryReader reader, long offset)
+        {
+            var prev = reader.BaseStream.Position;
+            reader.Seek(offset);
+            double ret = reader.ReadDouble();
+            reader.Seek(prev);
+            return ret;
+        }
+    }
+}
---
+++ BinaryExtensions/BinaryWriterExtensions.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BinaryExtensions
+{
+    public static class BinaryWriterExtensions
+    {
+        public static void Seek(this BinaryWriter writer, uint offset)
+        {
+            writer.Seek((long)offset);
+        }
+        public static void Seek(this BinaryWriter writer, long offset)
+        {
+            if (writer.BaseStream.Length <= offset)
+                throw new ArgumentOutOfRangeException("offset");
+            writer.BaseStream.Position = offset;
+        }
+        public static void WriteAt(this BinaryWriter writer, char c, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(c);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, bool b, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(b);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, byte b, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(b);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, byte[] b, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(b);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, sbyte sb, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(sb);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, short s, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(s);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, ushort us, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(us);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, int i, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(i);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, uint ui, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(ui);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, long l, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(l);
+            writer.Seek(prev);
+        }
+        public static void WriteAt(this BinaryWriter writer, ulong ul, long offset)
+        {
+            var prev = writer.BaseStream.Position;
+            writer.Seek(offset);
+            writer.Write(ul);
+            writer.Seek(prev);
+        }
+    }
+}
